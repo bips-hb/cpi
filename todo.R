15cf5f759902798a8a0ffb2283c2dda8670fae59
David Watson <david.s.watson11@gmail.com>
2022-02-09 15:35:33 +0000

    todo

---
+++ todo.R
@@ -0,0 +1,8 @@
+# Allow modular knockoff sampler
+# Allow for data frame input, factors, etc.
+# Allow alternative inference procedures
+# Update to mlr3 framework.
+# Update documentation, include examples, write vignette, etc.
+# Set up Github Actions
+# Add a gh-pages site
+# Implement group CPI option
