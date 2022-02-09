2338208e1ceaa5403bc13211f2afe8308deda5b0
David Watson <david.s.watson11@gmail.com>
2022-02-09 15:37:08 +0000

    todo fix

--- todo.R
+++ todo.R
@@ -2,7 +2,7 @@
 # Allow for data frame input, factors, etc.
 # Allow alternative inference procedures
 # Update to mlr3 framework.
-# Update documentation, include examples, write vignette, etc.
+# Write vignette
 # Set up Github Actions
 # Add a gh-pages site
 # Implement group CPI option
